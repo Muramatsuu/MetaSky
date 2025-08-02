37afc1b23298d00f7e65aaac6cb785a12258a5ea
Matsu <124514952+Muramatsuu@users.noreply.github.com>
2025-08-02 14:42:44 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/metasky/main.go
@@ -0,0 +1,23 @@
+// cmd/metasky/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"metasky/internal/metasky"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := metasky.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
